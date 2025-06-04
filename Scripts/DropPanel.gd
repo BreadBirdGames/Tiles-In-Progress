16dab964b46809a662ae4d8017ea34eb348a6368
cornelius <macrosedk@gmail.com>
2025-06-04 02:32:15 +0200

    add better water and make selection allowance better

--- Scripts/DropPanel.gd
+++ Scripts/DropPanel.gd
@@ -6,40 +6,72 @@ var camera_node: Camera2D = null
 export(NodePath) var tilemap = null
 var tilemap_node: TileMap = null
 
+export(NodePath) var grid = null
+var grid_node: Node2D = null
+
 export var dirt_tile_index = 0
-export var water_tile_index = 2
+export(PackedScene) var water_tile = null
+export(PackedScene) var enemy = null
 
 func _ready():
 	if typeof(get_node(camera)) != typeof(Camera2D):
 		printerr("camera_node = is not of type Camera2D")
 		return
-	
 	camera_node = get_node(camera) as Camera2D
 	
 	if typeof(get_node(tilemap)) != typeof(TileMap):
 		printerr("tilemap_node = is not of type TileMap")
 		return
-	
 	tilemap_node = get_node(tilemap) as TileMap
+	
+	if typeof(get_node(grid)) != typeof(Node2D):
+		printerr("grid_node = is not of type Node2D")
+		return
+	grid_node = get_node(grid) as Node2D
+
+func get_current_tile_pos():
+	var global_position = camera_node.get_global_mouse_position()
+	return tilemap_node.world_to_map(global_position)
 
 func can_drop_data(at_position, data):
-	return data.type == "DropItem"
+	if data.type != "DropItem":
+		return false
+	
+	var tilemap_position = get_current_tile_pos()
+	
+	if data.item_id == DragItem.Items.Water:
+		if tilemap_position.y < 0:
+			return false
+	else:
+		if tilemap_position.y > -1:
+			return false
+	
+	return true
 
 func drop_data(at_position, data):
-	var global_position = camera_node.get_global_mouse_position()
-	var tilemap_position = tilemap_node.world_to_map(global_position)
+	var tilemap_position = get_current_tile_pos()
 	
 	match data.item_id:
 		DragItem.Items.Dirt:
 			tilemap_node.set_cell(tilemap_position.x, tilemap_position.y, dirt_tile_index)
 			continue
 		DragItem.Items.Water:
-			tilemap_node.set_cell(tilemap_position.x, tilemap_position.y, water_tile_index)
+			var tile = water_tile.instance()
+			tile.global_position = tilemap_node.map_to_world(tilemap_position)
+			tilemap_node.add_child(tile)
 			continue
 		DragItem.Items.Monster:
-			tilemap_node.set_cell(tilemap_position.x, tilemap_position.y, water_tile_index)
+			var tile = enemy.instance()
+			tile.global_position = tilemap_node.map_to_world(tilemap_position)
+			tilemap_node.add_child(tile)
 			continue
 		DragItem.Items.None:
 			continue
 		_:
 			continue
+
+func _notification(what):
+	if what == NOTIFICATION_DRAG_BEGIN:
+		grid_node.show()
+	elif what == NOTIFICATION_DRAG_END:
+		grid_node.hide()
